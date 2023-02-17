b597fe59c294ca7319406400b6e531db9cb3f979
root <collinsmogaka99@gmail.comgit config user.name collinsmogakagit config user.email "collinsmogaka99@gmail.comgit config user.name "collinsmogaka>
2023-02-17 10:13:48 -0800

    m

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,20 +9,19 @@
 
 int main(void)
 {
-int n,last_digit;
-srand(time(0));
-n = rand();
-printf("Last digit of %d is ", n);
-
- last_digit = n % 10;
-
-if (last_digit > 5) {
-printf("and is greater than 5\n");
-} else if (last_digit == 0) {
-printf("and is 0\n");
+int n,lastd;
+n=rand() - RAND_MAX / 2;
+lastd = n % 10;
+if (lastd > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
 }
-else{
-printf("and is less than 6 and not 0\n");
+else if (lastd ==0) 
+{
+printf("Last digit of %d is %d and is 0\n", n, lastd);
 }
+else if (lastd <6 && lastd !=0)
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
 return (0);
 }
