46327f80ea81aa8d841d5511b9c1b56370e6177d
root <collinsmogaka99@gmail.comgit config user.name collinsmogakagit config user.email "collinsmogaka99@gmail.comgit config user.name "collinsmogaka>
2023-02-20 00:55:21 -0800

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 /**
@@ -10,10 +9,10 @@ int main(void)
 {
 char low;
 
-for (low ='a'; low <='z'; low++)
+for (low = 'a'; low <= 'z'; low++)
 putchar(low);
 
-for (low ='A'; low <='Z'; low++)
+for (low = 'A'; low <= 'Z'; low++)
 putchar(low);
 putchar('\n');
 
