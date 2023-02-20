6ac727e2b106cd0447bed63cf891c85a1918daed
root <collinsmogaka99@gmail.comgit config user.name collinsmogakagit config user.email "collinsmogaka99@gmail.comgit config user.name "collinsmogaka>
2023-02-19 22:13:13 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - main block
+*description: get a random number and print number
+*Return: Always (Success)
+*/
+
+int main() {
+int i;
+
+for (i = 97; i <= 122; i++) {
+putchar(i);
+}
+putchar(n);
+return 0;
+}
