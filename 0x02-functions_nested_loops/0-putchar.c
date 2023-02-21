6b720b723fe06c7aa832022f04f656ab369075d0
root <collinsmogaka99@gmail.comgit config user.name collinsmogakagit config user.email "collinsmogaka99@gmail.comgit config user.name "collinsmogaka>
2023-02-21 02:11:16 -0800

    loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - main block
+*description: get a random number and print number
+*Return: Always (Success)
+*/
+
+int main(void)
+
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include<unistd.h>
+
+/**
+* main - main block
+*description: get a random number and print number
+*Return: Always (Success)
+*/
+
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
