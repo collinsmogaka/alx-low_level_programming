8371eedb2128d64bc687f6b2771cdd7f3466549f
root <collinsmogaka99@gmail.comgit config user.name collinsmogakagit config user.email "collinsmogaka99@gmail.comgit config user.name "collinsmogaka>
2023-02-23 09:57:36 -0800

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*_isupper - Check if a letter is uppercase
+*@x: The number to be checked
+*Return: 1 for upper letter or 0 for any else
+*/
+
+int _isdigit(int x)
+{
+if (x >= 48 && x <= 57)
+{
+return (1);
+}
+return (0);
+}
